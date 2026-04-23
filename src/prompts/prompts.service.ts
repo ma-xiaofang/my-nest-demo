917509012e0f8594da01268b772a94fdfadc1df8
马小芳 <2493797657@qq.com>
2026-04-23 16:45:24 +0800

    feat(prompts): 添加多种提示词模板练习
    - 引入 ChatOpenAI 和 ConfigService 依赖
    - 使用注入装饰器注入配置服务
    - 配置 ChatOpenAI 对象，指定模型和 API 相关信息
    - 实现 PromptsService 构造函数初始化 ChatOpenAI 实例
    - 添加注释说明该服务用途

--- src/prompts/prompts.service.ts
+++ src/prompts/prompts.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { ChatOpenAI } from '@langchain/openai';
+import { ConfigService } from '@nestjs/config';
+/**
+ * 多种提示词模板练习
+ */
 @Injectable()
-export class PromptsService {}
+export class PromptsService {
+    @Inject()
+    private readonly configService: ConfigService;
+    constructor(
+        private readonly llm: ChatOpenAI
+    ) {
+        this.llm = new ChatOpenAI({
+            model: 'deepseek-chat', 
+            apiKey: this.configService.get('DEEPSEEK_API_KEY'),
+            configuration: { baseURL:'https://api.deepseek.com/v1'}
+        })
+    }
+    
+
+
+}
